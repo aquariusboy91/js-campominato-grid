6bdd7931028be984e7bfba0af00692fe24327a4e
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-12-01 15:20:56 +0100

    add arrays

--- js/script.js
+++ js/script.js
@@ -24,9 +24,43 @@
 
 let playfield = document.querySelector('.playfield');
 
+//creo un array di numeri random da 1 a 100
+let array_100= [];
+i = 0; 
+while (i < 100) {
+    let numero_rand = Math.floor(Math.random() * 100);
+     array_100.push(numero_rand);
+     i++;
+}
+console.log (array_100);
+
+//creo un array di numeri random da 1 a 81
+let array_81= [];
+i = 0; 
+while (i < 81) {
+    let numero_rand = Math.floor(Math.random() * 81);
+     array_81.push(numero_rand);
+     i++;
+}
+console.log (array_81);
+
+//creo un array di numeri random da 1 a 49
+let array_49= [];
+i = 0; 
+while (i < 49) {
+    let numero_rand = Math.floor(Math.random() * 49);
+     array_49.push(numero_rand);
+     i++;
+}
+console.log (array_49);
+
+
+
 //ciclo for che crea div da inserire in playflied
+//inserisce un numero da 1 a 100 in ogni div
 for(i = 0; i < 100; i++) {
     let square_div = document.createElement('div');
     square_div.classList.add("square-100");
     playfield.append(square_div);
+    square_div.innerHTML = array_100[i];
 }
